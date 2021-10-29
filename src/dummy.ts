a30e5b54ac9e915f763afbd892b73a9fa9606387
kbu715 <kbu715@gmail.com>
2021-10-29 21:39:10 +0900

    🍱 Upload dummy images and user data

---
+++ src/dummy.ts
@@ -0,0 +1,27 @@
+export const users: User[] = [
+  {
+    id: 0,
+    username: '김영철',
+    userImage: '/samples/img-user-01.jpg',
+  },
+  {
+    id: 1,
+    username: '개발자',
+    userImage: '/samples/img-user-02.jpg',
+  },
+  {
+    id: 2,
+    username: '코코',
+    userImage: '/samples/img-user-03.jpg',
+  },
+  {
+    id: 3,
+    username: '히히',
+    userImage: '/samples/img-user-04.jpg',
+  },
+  {
+    id: 4,
+    username: 'july',
+    userImage: '/samples/img-user-05.jpg',
+  },
+]
