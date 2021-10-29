e845ecc42d94d26a06da4f26e981145aca25138a
kbu715 <kbu715@gmail.com>
2021-10-29 22:52:43 +0900

    🚧 Create a custom hook: useClickOutside

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,8 +1,11 @@
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux'
 import type { RootState, AppDispatch } from '../app/store'
 
-export { useResponsive } from './useResponsive'
+export { useClickOutside } from './useClickOutside'
 export { useModals } from './useModals'
+export { usePrevious } from './usePrevious'
+export { useResponsive } from './useResponsive'
+export { useRouter } from './useRouter'
 
 // Use throughout your app instead of plain `useDispatch` and `useSelector`
 export const useAppDispatch = () => useDispatch<AppDispatch>()
